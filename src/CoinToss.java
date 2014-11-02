33e81dbcffa13f8c3104ecfa46969b636ea7275e
marioagarcia <betomg87@gmail.com>
2014-11-01 22:21:06 -0600

    backing up to gh

---
+++ src/CoinToss.java
@@ -0,0 +1,7 @@
+/**
+ * Created by mario on 10/30/14.
+ */
+public enum CoinToss {
+    HEADS,
+    TAILS
+}
---
+++ src/Observation.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mario on 10/30/14.
+ */
+public class Observation {
+
+    CoinToss coinTossOne;
+    CoinToss coinTossTwo;
+    CoinToss coinTossThree;
+}
